457cd032a634e5bdd22299c2515cca8ceff3a053
Your Name <234036564+sydbeausoleil@users.noreply.github.com>
2025-10-01 20:07:18 -0700

    Contents created

---
+++ src/data_analysis_functions.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""Basic Data Analysis Script"""
+
+def load_students(filename):
+    """Load student data from CSV."""
+    # TODO: Open file, read lines, skip header
+    # TODO: Split each line by comma
+    # TODO: Return list of student data
+    pass
+
+def calculate_average_grade(students):
+    """Calculate average grade."""
+    # TODO: Sum all grades
+    # TODO: Divide by number of students
+    pass
+
+def count_math_students(students):
+    """Count students in Math."""
+    # TODO: Count students where subject is Math
+    pass
+
+def generate_report(total, average, math_count):
+    """Generate report string."""
+    # TODO: Create formatted string with results
+    # TODO: Use f-strings with .1f for decimals
+    pass
+
+def save_report(report, filename):
+    """Save report to file."""
+    # TODO: Create output directory if needed
+    # TODO: Write report to file
+    pass
+
+def main():
+    # TODO: Load data
+    # TODO: Calculate statistics
+    # TODO: Generate and save report
+    pass
+
+if __name__ == "__main__":
+    main()
